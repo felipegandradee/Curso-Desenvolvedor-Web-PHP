021114e93ca4a2c2b76abb179cefeb67d01802be
Felipe Andrade <fga123@outlook.com>
2019-06-17 14:48:07 -0300

    Segundo Commit de Arquivos do PC - Empresa

--- conexao.php
+++ conexao.php
@@ -1,5 +1,4 @@
 <?php
-
 //credenciais de conexão
 $servidor = "localhost";
 $usuario = "root";
@@ -8,14 +7,4 @@ $dbname = "celke";
 
 //criando conexão
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
-
-//Verificando se deu certo a conexão
-if(!$conn){
-	die("Falha na conexão: " . mysqli_connect_error());
-
-} else {
-	echo "Conexão realizada com sucesso!";
-
-}
-
 ?>
